3bbd377fc0fa56ab747d9430ce4fd7a6ca25c582
wolfniNjja <98714945+wolfniNjja@users.noreply.github.com>
2022-01-30 22:43:15 +0100

    Update CharacterController2D.cs

--- CharacterController2D.cs
+++ CharacterController2D.cs
@@ -143,4 +143,10 @@ public class CharacterController2D : MonoBehaviour
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
-}
+} 
+                                                                                                                                                                             
+
+
+
+
+
